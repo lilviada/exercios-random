b9e0176aad07f22f8f86ababad29f6f13f1ddf15
lilvia <ananascsilva07@gmail.com>
2026-07-18 01:33:09 -0300

    Delete quest13.py

--- quest13.py
+++
@@ -1,8 +0,0 @@
-peixe = int(input())
-excesso = peixe - 50
-multa = excesso * 4
-
-if excesso > 0:
-    print("você excedeu", excesso, "quilos e terá de pagar", multa, "reais")
-else: 
-    print("você não execdeu o limite")
